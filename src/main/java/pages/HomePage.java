0bdda4b031bfd23eebf9e28bc2bebf1941811892
Sondos_Nasser <sondosnasser08@gmail.com>
2023-10-24 18:13:11 +0300

    fcopy

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.time.LocalDate;
+import java.time.Month;
 import java.time.format.DateTimeFormatter;
 
 public class HomePage extends PageBase {
@@ -22,7 +23,7 @@ public class HomePage extends PageBase {
     //action
 
     public void SelectDate() {
-        expwait();
+         expwait();
         click1(checkInBtn);
         expwait();
 
@@ -40,29 +41,92 @@ public class HomePage extends PageBase {
         String[] partsss = strDay.split("2023-");
         String striDay = partsss[1];
 
+        Month compairMonthsStr1 = localDate.getMonth();
+        System.out.println(compairMonthsStr1);
+
+
+
+
+
+
         var daaay = "[data-testid=\"" + striDay + "-daytext\"]";
         //System.out.println(daaay);
         var day = By.cssSelector("div[class=\"dg7nl1z dir dir-ltr\"] " + daaay);
 
+        expwait();
+        click1(day);
+        expwait();
+
         LocalDate localDate1 = LocalDate.now().plusDays(20);
         String strDay2 = localDate1.toString();
-        System.out.println(strDay2);
 
         String[] partssss = strDay2.split("2023-");
         String striiDay = partssss[1];
 
+        Month compairMonthsStr2 = localDate1.getMonth();
+        System.out.println(compairMonthsStr2);
+
+
         var daaayy = "[data-testid=\"" + striiDay + "-daytext\"]";
-        //System.out.println(daaayy);
         expwait();
         var dayy = By.cssSelector("div[class=\"dg7nl1z dir dir-ltr\"] " + daaayy);
-        expwait();
 
-        click1(day);
 
-        expwait();
-        click1(rightRow);
+
+
+        int c = switch (compairMonthsStr1) {
+            case JANUARY -> 1;
+            case FEBRUARY -> 2;
+            case MARCH -> 3;
+            case APRIL -> 4;
+            case MAY -> 5;
+            case JUNE -> 6;
+            case JULY -> 7;
+            case AUGUST -> 8;
+            case SEPTEMBER -> 9;
+            case OCTOBER -> 10;
+            case NOVEMBER -> 11;
+            case DECEMBER -> 12;
+        };
+
+        int f = switch (compairMonthsStr2) {
+            case JANUARY -> 1;
+            case FEBRUARY -> 2;
+            case MARCH -> 3;
+            case APRIL -> 4;
+            case MAY -> 5;
+            case JUNE -> 6;
+            case JULY -> 7;
+            case AUGUST -> 8;
+            case SEPTEMBER -> 9;
+            case OCTOBER -> 10;
+            case NOVEMBER -> 11;
+            case DECEMBER -> 12;
+        };
+
+        System.out.println(c);
+        System.out.println(f);
+
+
+        if ( c == f) {
+            expwait();
+            click1(dayy);
+            expwait();
+
+        } else if (c < f) {
+
+
+            for (int i = c; i < f; i++) {
+
+                expwait();
+                click1(rightRow);
+                expwait();
+            }
+
+        }
         expwait();
         click1(dayy);
+        expwait();
 
         new HomePage(driver);
 
